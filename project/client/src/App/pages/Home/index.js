5664ea2ce060d7c9b6c9415cc693bf9b99859a8a
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-08-21 20:24:52 -0300

    add Header

--- project/client/src/App/pages/Home/index.js
+++ project/client/src/App/pages/Home/index.js
@@ -8,6 +8,12 @@ function Home() {
     <Conteiner className="Home">
       <Head htmlAttributes={{ lang: 'pt-br' }}>
         <title>TheNews - Home</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <Header />
