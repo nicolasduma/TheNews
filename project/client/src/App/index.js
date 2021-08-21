6bae984bc94b9911aa046d9981ffb7569146f0f5
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-08-21 20:24:43 -0300

    add fonts

--- project/client/src/App/index.js
+++ project/client/src/App/index.js
@@ -2,6 +2,9 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 import './styles/index.css'
 
+import './assets/fonts/styles.css'
+import './assets/fonts/tommy-regular.woff2'
+
 import { Conteiner } from './components'
 import Routes from './Routes'
 
