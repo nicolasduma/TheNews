6f918d66c1d69354cfdb6e8aa85069ffa2f1f812
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-09-11 17:48:05 -0300

    update news page

--- project/src/App/Routes.js
+++ project/src/App/Routes.js
@@ -1,12 +1,13 @@
 import { Route, Switch } from 'react-router-dom'
 
-import { Home } from './pages'
+import { Home, News } from './pages'
 
 function Routes() {
   return (
     <main>
       <Switch>
         <Route exact path={['/', '/home']} component={Home} />
+        <Route exact path={'/news/:id'} component={News} />
       </Switch>
     </main>
   )
--- project/src/App/pages/News/index.js
+++ project/src/App/pages/News/index.js
@@ -1,6 +1,22 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
 import { Conteiner } from './styles'
+import get from '../../services/news/get'
 
 function News() {
+  function getNews() {
+    const vasco = useParams
+    console.log(vasco())
+  }
+  const [news, setNews] = useState({})
+
+  useEffect(() => {
+    // const update = () => setNews(GetNews())
+    // update()
+    getNews()
+  }, [])
+
   return <Conteiner></Conteiner>
 }
 
