38f8b826ebec880a613a3aaa69b0cc4c7326a7cd
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-09-25 18:05:16 -0300

    update get news

--- project/src/App/services/news/get.js
+++ project/src/App/services/news/get.js
@@ -6,13 +6,21 @@ const all = async () => {
   const snapshot = await newsCollection.get()
   let docs = []
 
-  snapshot.forEach(doc => docs.push({ _id: doc.id, ...doc.data() }))
-  return docs
+  snapshot.forEach(doc => {
+    const data = doc.data()
+    data.created = new Date(data.created.seconds * 1000)
+    return docs.push({ _id: doc.id, ...data })
+  })
+
+  console.log(docs)
+
+  return docs.sort((a, b) => {
+    return a.created > b.created ? -1 : a.created < b.created ? 1 : 0
+  })
 }
 
 const newsById = async id => {
   const snapshot = await newsCollection.doc(id).get()
-  console.log(snapshot)
   return { _id: snapshot.id, ...snapshot.data() }
 }
 
