d550deb4d2f3cf6841d61e583241d5c47a2446ff
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-10-19 19:44:10 -0300

    update AddNews style

--- project/src/App/pages/AddNews/styles.js
+++ project/src/App/pages/AddNews/styles.js
@@ -2,19 +2,24 @@ import styled from 'styled-components'
 
 export const Conteiner = styled.div`
   width: 100%;
-  height: 100vh;
+  padding: 30px 0;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
+  gap: 30px;
 `
 
-const Label = styled.label`
-  font-size: 2.2rem;
+export const Label = styled.label`
+  margin-bottom: 8px;
+  display: block;
+  font-size: 1.8rem;
+  cursor: pointer;
 `
 
 const inputAndSelect = `
-  width: 40vw;
+  width: 60vw;
+  padding: 5px 5px;
+  font-size: 1.7rem;
 `
 
 export const Select = styled.select`
@@ -23,4 +28,17 @@ export const Select = styled.select`
 
 export const Input = styled.input`
   ${inputAndSelect}
+`
+
+export const TextArea = styled.textarea`
+  max-width: 60vw;
+  min-width: 60vw;
+  min-height: 150px;
+  max-height: 250px;
+`
+
+export const Button = styled.button`
+  font-size: 1.6rem;
+  padding: 10px 15px;
+  cursor: pointer;
 `
