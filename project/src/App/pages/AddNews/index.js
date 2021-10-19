9b5083afc65baf67698bcf04f445378f60ad7bff
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-10-19 19:44:25 -0300

    update AddNews page

--- project/src/App/pages/AddNews/index.js
+++ project/src/App/pages/AddNews/index.js
@@ -1,34 +1,90 @@
-import { Head } from '../../components'
+import { useEffect, useState } from 'react'
 
-import { Conteiner, Select, Input } from './styles'
+import { Head } from '../../components'
+import { Conteiner, Label, Select, Input, TextArea, Button } from './styles'
+
+import get from '../../services/categories/get'
 
 function AddNews() {
+  const [categories, setCategories] = useState([])
+
+  const [valueCategory, setValueCategory] = useState('')
+  const [valueTitle, setValueTitle] = useState('')
+  const [valueDescription, setValueDescription] = useState('')
+  const [valueSource, setValueSource] = useState('')
+
+  useEffect(() => {
+    const update = async () => setCategories(await get())
+    update()
+  }, [])
+
+  useEffect(() => {
+    if (categories[0]) setValueCategory(categories[0])
+  }, [categories])
+
   return (
     <Conteiner className="AddNews">
       <Head htmlAttributes={{ lang: 'pt-br' }} title="TheNews - adicionar notícia" />
       <div>
-        <h3>Categoria *</h3>
-        <Select>
-          <option>Esporte</option>
+        <Label htmlFor="select-category">Categoria *</Label>
+        <Select
+          id="select-category"
+          value={valueCategory}
+          onChange={event => setValueCategory(event.target.value)}
+        >
+          {categories[0]
+            ? categories.map(category => (
+                <option
+                  key={`option-${category}`}
+                  value={category}
+                  children={category}
+                  defaultValue
+                />
+              ))
+            : ''}
         </Select>
       </div>
 
       <div>
-        <h3>Título *</h3>
-        <Input type="text" maxLength={40} />
+        <Label htmlFor="input-title">Título *</Label>
+        <Input
+          id="input-title"
+          type="text"
+          maxLength={40}
+          value={valueTitle}
+          onChange={event => setValueTitle(event.target.value)}
+        />
       </div>
 
       <div>
-        <h3>Descrição *</h3>
-        <Input type="text" maxLength={1000} />
+        <Label htmlFor="text-description">Descrição *</Label>
+        <TextArea
+          id="text-description"
+          type="text"
+          maxLength={1000}
+          required
+          value={valueDescription}
+          onChange={event => setValueDescription(event.target.value)}
+        />
       </div>
 
       <div>
-        <h3>Fonte *</h3>
-        <Input type="text" maxLength={20} />
+        <Label htmlFor="input-source">Fonte *</Label>
+        <Input
+          id="input-source"
+          type="text"
+          maxLength={20}
+          value={valueSource}
+          onChange={event => setValueSource(event.target.value)}
+        />
       </div>
 
-      <button>Enviar</button>
+      <Button
+        onClick={() => {
+          console.log(valueCategory, valueTitle, valueDescription, valueSource)
+        }}
+        children="Enviar"
+      />
     </Conteiner>
   )
 }
