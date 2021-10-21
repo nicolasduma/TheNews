d07eac871511ff0b8462d0627ff7805eadc89d67
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-10-21 18:50:12 -0300

    update AddNews page

--- project/src/App/pages/AddNews/index.js
+++ project/src/App/pages/AddNews/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 
 import { Head } from '../../components'
-import { Conteiner, Label, Select, Input, TextArea, Button } from './styles'
+import { Conteiner, Title, Form, Label, Select, Input, TextArea, Button } from './styles'
 
 import get from '../../services/categories/get'
 
@@ -25,66 +25,73 @@ function AddNews() {
   return (
     <Conteiner className="AddNews">
       <Head htmlAttributes={{ lang: 'pt-br' }} title="TheNews - adicionar notícia" />
-      <div>
-        <Label htmlFor="select-category">Categoria *</Label>
-        <Select
-          id="select-category"
-          value={valueCategory}
-          onChange={event => setValueCategory(event.target.value)}
-        >
-          {categories[0]
-            ? categories.map(category => (
-                <option
-                  key={`option-${category}`}
-                  value={category}
-                  children={category}
-                  defaultValue
-                />
-              ))
-            : ''}
-        </Select>
-      </div>
 
-      <div>
-        <Label htmlFor="input-title">Título *</Label>
-        <Input
-          id="input-title"
-          type="text"
-          maxLength={40}
-          value={valueTitle}
-          onChange={event => setValueTitle(event.target.value)}
-        />
-      </div>
-
-      <div>
-        <Label htmlFor="text-description">Descrição *</Label>
-        <TextArea
-          id="text-description"
-          type="text"
-          maxLength={1000}
-          required
-          value={valueDescription}
-          onChange={event => setValueDescription(event.target.value)}
-        />
-      </div>
-
-      <div>
-        <Label htmlFor="input-source">Fonte *</Label>
-        <Input
-          id="input-source"
-          type="text"
-          maxLength={20}
-          value={valueSource}
-          onChange={event => setValueSource(event.target.value)}
-        />
-      </div>
-
-      <Button
-        onClick={() => {
+      <Title>Adicionar Notícia</Title>
+      <Form
+        onSubmit={event => {
+          event.preventDefault()
           console.log(valueCategory, valueTitle, valueDescription, valueSource)
         }}
-        children="Enviar"
-      />
+      >
+        <div>
+          <Label htmlFor="select-category">Categoria *</Label>
+          <Select
+            id="select-category"
+            required
+            value={valueCategory}
+            onChange={event => setValueCategory(event.target.value)}
+          >
+            {categories[0]
+              ? categories.map(category => (
+                  <option
+                    key={`option-${category}`}
+                    value={category}
+                    children={category}
+                    defaultValue
+                  />
+                ))
+              : ''}
+          </Select>
+        </div>
+
+        <div>
+          <Label htmlFor="input-title">Título *</Label>
+          <Input
+            id="input-title"
+            type="text"
+            maxLength={40}
+            required
+            value={valueTitle}
+            onChange={event => setValueTitle(event.target.value)}
+          />
+        </div>
+
+        <div>
+          <Label htmlFor="text-description">Descrição *</Label>
+          <TextArea
+            id="text-description"
+            type="text"
+            maxLength={1000}
+            required
+            value={valueDescription}
+            onChange={event => setValueDescription(event.target.value)}
+          />
+        </div>
+
+        <div>
+          <Label htmlFor="input-source">Fonte *</Label>
+          <Input
+            id="input-source"
+            type="text"
+            maxLength={20}
+            required
+            value={valueSource}
+            onChange={event => setValueSource(event.target.value)}
+          />
+        </div>
+
+        <Button type="submit" children="Enviar" />
+      </Form>
     </Conteiner>
   )
 }
--- project/src/App/pages/AddNews/styles.js
+++ project/src/App/pages/AddNews/styles.js
@@ -9,28 +9,45 @@ export const Conteiner = styled.div`
   gap: 30px;
 `
 
+export const Title = styled.h1`
+  font-size: 2rem;
+`
+
+export const Form = styled.form`
+  width: 100%;
+  padding: 30px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+`
+
 export const Label = styled.label`
   margin-bottom: 8px;
   display: block;
   font-size: 1.8rem;
   cursor: pointer;
 `
-
-const inputAndSelect = `
-  width: 60vw;
+const selectInputAndTextArea = `
   padding: 5px 5px;
   font-size: 1.7rem;
 `
 
+const selectAndInput = `
+  ${selectInputAndTextArea}
+  width: 60vw;
+`
+
 export const Select = styled.select`
-  ${inputAndSelect}
+  ${selectAndInput}
 `
 
 export const Input = styled.input`
-  ${inputAndSelect}
+  ${selectAndInput}
 `
 
 export const TextArea = styled.textarea`
+  ${selectInputAndTextArea}
   max-width: 60vw;
   min-width: 60vw;
   min-height: 150px;
