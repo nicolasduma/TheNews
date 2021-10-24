1dc026b90c43a3f45c5e8f9bef15d94512b63732
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-10-23 21:22:16 -0300

    update sendNews

--- project/src/App/pages/AddNews/functions/sendNews.js
+++ project/src/App/pages/AddNews/functions/sendNews.js
@@ -1,1 +1,20 @@
-export default function sendNews()
+import add from '../../../services/news/add'
+
+const addNews = async (valueCategory, valueTitle, valueDescription, valueSource) => {
+  const news = {
+    category: valueCategory,
+    title: valueTitle,
+    description: valueDescription,
+    source: valueSource,
+    created: new Date(),
+  }
+
+  try {
+    await add(news)
+    alert('Noticia adicionada com sucesso!')
+  } catch {
+    alert('Houve um erro, e não foi possível adicionar a notícia!')
+  }
+}
+
+export default addNews
