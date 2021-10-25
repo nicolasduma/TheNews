04d0eeccedef4859506721f1f2fed19f6ae5e4f9
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-10-24 22:25:32 -0300

    update sendNews

--- project/src/App/pages/AddNews/functions/sendNews.js
+++ project/src/App/pages/AddNews/functions/sendNews.js
@@ -7,6 +7,8 @@ const addNews = async (valueCategory, valueTitle, valueDescription, valueSource)
     description: valueDescription,
     source: valueSource,
     created: new Date(),
+    imageSrc:
+      'https://firebasestorage.googleapis.com/v0/b/thenews-duma.appspot.com/o/images%2Fnewspaper.jpg?alt=media&token=a08b4fa1-488c-476d-8c9c-9ee47a0720b4',
   }
 
   try {
