f3285c9b7b29ee7d6f562634a971d7b5879b08c5
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-11-02 22:52:13 -0300

    update add news sevices

--- project/src/App/services/news/add.js
+++ project/src/App/services/news/add.js
@@ -1,7 +1,14 @@
-import { db } from '../../firebase'
+import { db, storage } from '../../firebase'
 
 const newsCollection = db.collection('news')
+const storageRef = storage.ref('/Images')
 
 const add = async news => await newsCollection.add(news)
+
+export const addCover = async fileBase64 => {
+  const snapshot = await storageRef.child('Cover.jpg').putString(fileBase64, 'base64')
+
+  return snapshot
+}
 
 export default add
