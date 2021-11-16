4476f5b29724de453f5306bccafa3da3f6cd02f2
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-11-15 22:53:13 -0300

    update delete service

--- project/src/App/services/categories/delete.js
+++ project/src/App/services/categories/delete.js
@@ -1,5 +1,8 @@
+import { db } from '../../firebase'
+import { doc } from 'firebase/firestore'
+
 const deleteCategory = async id => {
-  console.log(id)
+  console.log(doc(db, 'categories', id))
 }
 
 export default deleteCategory
