979ef85c328cf253a62cb2b3dbe0e59d212800bd
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-11-17 20:11:51 -0300

    update delete categories

--- project/src/App/services/categories/delete.js
+++ project/src/App/services/categories/delete.js
@@ -1,8 +1,17 @@
 import { db } from '../../firebase'
-import { doc } from 'firebase/firestore'
 
-const deleteCategory = async id => {
-  console.log(doc(db, 'categories', id))
+const deleteCategory = async (id, categories, setCategories, lockedFunction, setLockedFunction) => {
+  if (!lockedFunction) {
+    setLockedFunction(true)
+    try {
+      await db.collection('categories').doc(id).delete()
+      setCategories(categories.filter(category => category[0] !== id))
+      setLockedFunction(false)
+    } catch (error) {
+      alert(`Não foi possivel deletar a categoria \n${error}`)
+      setLockedFunction(false)
+    }
+  } else alert('Por favor, aguarde!')
 }
 
 export default deleteCategory
