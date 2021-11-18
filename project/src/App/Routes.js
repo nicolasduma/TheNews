a647976ec54ae87dbdb80a9f435c0447970fb9cd
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-11-18 17:05:56 -0300

    update pages

--- project/src/App/Routes.js
+++ project/src/App/Routes.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import { Home, News, UserAdmin, AddNews, ListCategories } from './pages'
+import { Home, News, UserAdmin, AddNews, ListCategories, AddCategory } from './pages'
 
 function Routes({ states }) {
   return (
@@ -17,6 +17,7 @@ function Routes({ states }) {
           <Route exact path={'/app/user/'} component={UserAdmin} />
           <Route exact path={'/app/user/news/add'} component={AddNews} />
           <Route exact path={'/app/user/categories'} component={ListCategories} />
+          <Route exact path={'/app/user/categories/add'} component={AddCategory} />
         </Switch>
       </main>
     </Router>
