e0e847d08004985b431904d21f0634613ad98331
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-11-20 21:28:42 -0300

    update ListNews page

--- project/src/App/pages/ListNews/index.js
+++ project/src/App/pages/ListNews/index.js
@@ -1,10 +1,27 @@
-import { Conteiner } from './styled'
+import { useState, useEffect } from 'react'
+
+import { Conteiner, Content, NewsConteiner } from './styled'
 import { Head } from '../../components'
+import get from '../../services/news/get'
 
 function ListNews() {
+  const [allNews, setAllNews] = useState([])
+
+  useEffect(() => {
+    const update = async () => setAllNews(await get.all())
+    update()
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <Conteiner>
       <Head title="The News - Lista de Notícias" />
+      <Content>
+        {allNews.map(news => (
+          <NewsConteiner key={news._id}>{news.title}</NewsConteiner>
+        ))}
+      </Content>
     </Conteiner>
   )
 }
--- project/src/App/pages/ListNews/styled.js
+++ project/src/App/pages/ListNews/styled.js
@@ -3,3 +3,5 @@ import styled from 'styled-components'
 export const Conteiner = styled.div``
 
 export const Content = styled.ul``
+
+export const NewsConteiner = styled.li``
