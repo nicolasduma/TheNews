590b4c5c845bef5fa784db1b275c7e9c6af3e81d
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-11-21 16:42:57 -0300

    update ListNews page

--- project/src/App/pages/ListNews/index.js
+++ project/src/App/pages/ListNews/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-import { Conteiner, Content, NewsConteiner } from './styled'
+import { Conteiner, Content, NewsConteiner, TextConteiner, TextId, Title, Button } from './styled'
 import { Head } from '../../components'
 import get from '../../services/news/get'
 
@@ -18,9 +18,17 @@ function ListNews() {
     <Conteiner>
       <Head title="The News - Lista de Notícias" />
       <Content>
-        {allNews.map(news => (
-          <NewsConteiner key={news._id}>{news.title}</NewsConteiner>
-        ))}
+        {allNews[0]
+          ? allNews.map(news => (
+              <NewsConteiner key={news._id}>
+                <TextConteiner>
+                  <TextId children={news._id} />
+                  <Title children={news.title} />
+                </TextConteiner>
+                <Button children="Deletar" />
+              </NewsConteiner>
+            ))
+          : ''}
       </Content>
     </Conteiner>
   )
--- project/src/App/pages/ListNews/styled.js
+++ project/src/App/pages/ListNews/styled.js
@@ -1,7 +1,41 @@
 import styled from 'styled-components'
+import { fonts } from '../../styles'
 
 export const Conteiner = styled.div``
 
-export const Content = styled.ul``
+export const Content = styled.ul`
+  padding: 20px 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`
 
-export const NewsConteiner = styled.li``
+export const NewsConteiner = styled.li`
+  display: flex;
+  justify-content: space-between;
+  padding: 15px;
+  border-bottom: solid 1px #000;
+`
+
+export const TextConteiner = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+export const TextId = styled.h3`
+  font-family: ${fonts.poppins};
+  font-size: 1.3rem;
+  padding-right: 20px;
+`
+
+export const Title = styled.h2`
+  border-left: solid 1px black;
+  font-size: 2rem;
+  padding-left: 20px;
+`
+
+export const Button = styled.button`
+  padding: 2px 5px;
+  font-size: 1.6rem;
+  cursor: pointer;
+`
