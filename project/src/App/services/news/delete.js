4936a79d48aa91cb15b879f9352be6d75d6d44ef
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-11-23 21:18:45 -0300

    update delete news service

--- project/src/App/services/news/delete.js
+++ project/src/App/services/news/delete.js
@@ -6,7 +6,7 @@ const deleteNews = async (id, allNews, setAllNews, lockedFunction, setLockedFunc
     try {
       await db.collection('news').doc(id).delete()
       alert('Notícia deletada com sucesso!')
-      setAllNews(allNews.filter(news => news._id != id))
+      setAllNews(allNews.filter(news => news._id !== id))
       setLockedFunction(false)
     } catch (error) {
       alert(`Não foi possivel deletar a categoria \n${error}`)
