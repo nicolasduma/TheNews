741baa0e056f1735b8028ef6cd28c82637a5d6dc
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-12-01 19:59:45 -0300

    update add category service

--- project/src/App/services/categories/add.js
+++ project/src/App/services/categories/add.js
@@ -1,10 +1,10 @@
 import { db } from '../../firebase'
 
-function add(lockedFunction, setLockedFunction, stringValue) {
+async function add(lockedFunction, setLockedFunction, stringValue) {
   if (!lockedFunction) {
     setLockedFunction(true)
     try {
-      db.collection('categories').add({ category: stringValue })
+      await db.collection('categories').add({ category: stringValue })
       alert('Categoria adicionada!')
       setLockedFunction(false)
     } catch (error) {
